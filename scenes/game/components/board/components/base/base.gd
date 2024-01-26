0d3f445d6c1bd92f1ed0d00fb28087cd923b1f80
D2 <montecastrodaniel@gmail.com>
2024-01-26 19:02:20 +0800

    [DRAG] Updated dragging functions

--- scenes/game/components/board/components/base/base.gd
+++ scenes/game/components/board/components/base/base.gd
@@ -1,35 +1,38 @@
 extends TileMap
 class_name Base
+## The spawn-points for the [Unit]s to be set on the [Field].[br]
+## Allows the [USER] to drag-and-drop said [Units] to set-up their formation.
 
 
-func _ready():
-	setup_ready()
-
-
-func setup_ready():
-	for child: Unit in get_children():
-		var coords = local_to_map(child.position)
-		toggle_cell(coords)
-
-func toggle_cell(coords: Vector2i):
-	var data: TileData = get_cell_tile_data(0, coords)
-	if data:
-		var filled: bool = data.get_custom_data('filled')
-		data.set_custom_data('filled', !filled)
-
-
-
-enum LAYERS { BASE, BOARD }
+# Constants
+const DATA = 'filled'
 const COLORS = {
 	DEF = Color(0,0,0,0),
 	DARK = Color(0,0,0,0.5),
 	LITE = Color(1,1,1,0.5)
 }
 
-func reset(): set_layer_modulate(LAYERS.BOARD, COLORS.DEF)
-func darken(): set_layer_modulate(LAYERS.BOARD, COLORS.DARK)
-func lighten(): set_layer_modulate(LAYERS.BOARD, COLORS.LITE)
+## 
+enum LAYERS { BASE, BOARD }
 
+
+# Core Functions ------------------------------------------------------------- #
+
+func _ready():
+	SIGNALS.drag_to.connect(drag_unit)
+	update()
+
+
+## Resets the modulation for the [TileMap].
+func _reset(): set_layer_modulate(LAYERS.BOARD, COLORS.DEF)
+
+
+# Color Handling ------------------------------------------------------------- #
+
+#func darken(): set_layer_modulate(LAYERS.BOARD, COLORS.DARK)
+#func lighten(): set_layer_modulate(LAYERS.BOARD, COLORS.LITE)
+
+## Brightens or darkens the [TileMap] based on the current modulation.
 func toggle():
 
 	var color = COLORS.DEF
@@ -40,4 +43,49 @@ func toggle():
 	
 	set_layer_modulate(LAYERS.BOARD, color)
 	print("%s: %s" % [self.name, self.modulate])
-	
+
+
+# Drag-based Functions ------------------------------------------------------- # 
+
+## Marks all child [Draggable]s' cells on the custom [member Base.DATA] layer.
+func update():
+	for child: Draggable in get_children():
+		var cell = local_to_map(child.position)
+		toggle_cell(cell)
+
+
+## Toggles the given [param cell] on the custom [member Base.DATA] layer.
+func toggle_cell(cell: Vector2i):
+	var data: TileData = get_cell_tile_data(0, cell)
+	if data:
+		var filled: bool = data.get_custom_data(DATA)
+		data.set_custom_data(DATA, !filled)
+
+
+## Drags the child [param unit] towards the given [param pos], if possible.[br]
+## If the drag is invalid, forces a [method Draggable.snap_back].
+func drag_unit(unit: Draggable, pos: Vector2):
+
+	if unit.get_parent() == self:
+
+		var cell := local_to_map(pos)
+		var data := get_cell_tile_data(0, cell)
+
+		if data:
+
+			var filled: bool = data.get_custom_data(DATA)
+
+			if not filled:
+
+				data.set_custom_data(DATA, !filled)
+
+				var from := local_to_map(unit.get_position())
+				toggle_cell(from)
+
+				var to := to_global(map_to_local(cell))
+				unit.set_global_position(to)
+				unit.originate(to)
+				
+				return
+
+	unit.snap_back()
--- scenes/game/components/unit/scripts/drag.gd
+++ scenes/game/components/unit/scripts/drag.gd
@@ -1,92 +1,101 @@
 extends Unit
 class_name Draggable
+## Allows drag-and-drop functionality for the [Unit] class.[br]
+## This [Draggable] can only be placed within its respective [Base].
 
+
+# Constants
+const INPUT = 'Click'
+const UNITS = 'Units'
+const ALLIES = 'Allies'
+
+
+## Parent Base
 @onready var BASE: Base = get_parent()
-@onready var START = global_position
-@onready var ORIGIN = START
 
-@onready var MIN: Vector2i = Vector2i.ZERO
-@onready var MAX: Vector2i = Vector2i.ZERO
 
-# Basic Functions ------------------------------------------------------------------------------- #
+## Initial global position where the [Unit] is spawned from.
+@onready var START := global_position
+
+
+## Global position to [method Draggable.snap_back] to.
+@onready var ORIGIN := START
+
+
+# Core Functions ------------------------------------------------------------- #
 
 func _ready():
-	connect("input_event", Callable(self, "drag_unit"))
-	add_to_group("Allies", true)
-	add_to_group("Units", true)
+	self.input_event.connect(drag)
+	add_to_group(ALLIES, true)
+	add_to_group(UNITS, true)
 	set_process(false)
 
-func _start():
-	disconnect("input_event", Callable(self, "drag_unit"))
 
-func _reset(): set_global_position(START)
+## Moves the [Unit] back to its [member DRAG.START] position.
+func _start(): set_global_position(START)
 
 
-# Initalizing Origin (for resetting Drag-&-Drop) ------------------------------------------------ #
+## Disables the ability to do [method Draggable.drag] this [Unit].
+func _reset(): self.input_event.disconnect(drag)
 
-func originate(pos: Vector2):
-	ORIGIN = global_position
-	set_position(pos)
 
-# Drag-&-Drop Functions ------------------------------------------------------------------------- #
-
+## When enabled, binds the [Unit] to the global mouse position.
 func _process(delta):
 	var mouse_pos = get_global_mouse_position()
 	var new_pos = lerp(global_position, mouse_pos, 25 * delta)
 	set_global_position(new_pos)
 
 
-const GROUP = "Units"
-func toggle_drag(val: bool):
+# Drag-&-Drop Functions ------------------------------------------------------ #
+
+## Sets the [method Draggable.snap_back] location for this unit, in case
+## [method Draggable.drag] is cancelled or [method Draggable.snap_back] fails.
+func originate(pos: Vector2):
+	ORIGIN = global_position
+	set_position(pos)
+
+
+## Attaches the [Unit] to the mouse via [method Draggable._process].
+func shadow(val: bool):
 
 	if val:
-		Anim.z_index = 15
-		if GROUP in get_groups():
-			remove_from_group(GROUP)
+		ANIM.z_index = 15
+		if UNITS in get_groups():
+			remove_from_group(UNITS)
 	else:
-		Anim.z_index = 0
-		add_to_group(GROUP)
+		ANIM.z_index = 0
+		add_to_group(UNITS)
 
-	get_tree().call_group(GROUP, "mouse_toggle", !val)
+	get_tree().call_group(UNITS, 'mouse_toggle', !val)
 	set_process(val)
 
 
-func drag_unit(_vp, _event, _idx):
+## Controls mouse interactions with the [Unit].
+func drag(_vp, _event, _idx):
 
-	if not USER.READIED:
+	if not USER.IN_GAME:
 
-		var locked = is_processing()
+		var locked := is_processing()
 
-		if not locked and Input.is_action_just_pressed("Click"):
-			toggle_drag(true)
+		## Enable [method Draggable.shadow]
+		if not locked and Input.is_action_just_pressed(INPUT):
+			shadow(true)
 
-		elif locked and Input.is_action_just_released("Click"):
-			toggle_drag(false)
-			if overlaps_body(BASE): snap_to_grid()
+		## Disable [method Draggable.shadow] and "drop" [Unit].
+		elif locked and Input.is_action_just_released(INPUT):
+			shadow(false)
+
+			if overlaps_body(BASE): snap_to()
 			else: snap_back()
 
-# TileMap Positioning Functions ----------------------------------------------#
 
+# Positioning Functions ------------------------------------------------------ #
+
+## Repositions the [Draggable] to its [member Draggable.ORIGIN].
 func snap_back(): set_global_position(ORIGIN)
 
-func snap_to_grid():
 
-	var coords: Vector2 = BASE.local_to_map(get_global_mouse_position())
-	var data: TileData = BASE.get_cell_tile_data(0, coords)
-
-	if data:
-
-		var filled: bool = data.get_custom_data('filled')
-
-		if filled: snap_back()
-			
-		else:
-
-			data.set_custom_data('filled', !filled)
-			var from: Vector2 = BASE.local_to_map(get_position())
-			BASE.toggle_cell(from)
-
-			set_position(BASE.map_to_local(coords))	
-			ORIGIN = global_position
-
-	else: snap_back()
+## Attempts to position the [Draggable] to the current mouse position.
+func snap_to():
+	var pos := get_global_mouse_position()
+	SIGNALS.drag_to.emit(self, pos)
